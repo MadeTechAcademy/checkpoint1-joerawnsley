8cb1a48e278209132743a649b6568e914752854d
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-11-24 13:17:43 +0000

    add passing test to check that a selection of correct duties are printed to stdout. refactor tests to avoid repetition

--- test_themes.py
+++ test_themes.py
@@ -1,14 +1,27 @@
 from themes import list_of_duties
+from themes import print_duties_to_terminal
 
-class TestListOfDuties():
-        def test_length(self):
-            assert len(list_of_duties) == 13
+
+
+duty_3 = "Duty 3 Engage in productive pair/mob programming to underpin the practice of peer review."
+duty_8 = "Duty 8 Evolve and define architecture, utilising the knowledge and experience of the team to design in an optimal user experience, scalability, security, high availability and optimal performance."
+duty_13 = "Duty 13 Accept ownership of changes; embody the DevOps culture of 'you build it, you run it', with a relentless focus on the user experience."
         
-        def test_includes_duties(self):
-            duty_3 = "Duty 3 Engage in productive pair/mob programming to underpin the practice of peer review."
-            duty_8 = "Duty 8 Evolve and define architecture, utilising the knowledge and experience of the team to design in an optimal user experience, scalability, security, high availability and optimal performance."
-            duty_13 = "Duty 13 Accept ownership of changes; embody the DevOps culture of 'you build it, you run it', with a relentless focus on the user experience."
-            assert duty_3 in list_of_duties
-            assert duty_8 in list_of_duties
-            assert duty_13 in list_of_duties
+class TestListOfDuties():
+    def test_length(self):
+        assert len(list_of_duties) == 13
+    
+    def test_includes_duties(self):
+        assert duty_3 in list_of_duties
+        assert duty_8 in list_of_duties
+        assert duty_13 in list_of_duties
 
+
+class TestPrintFunction():
+    def test_check_capsys(self, capsys):
+        print_duties_to_terminal()
+        captured = capsys.readouterr()
+
+        assert duty_3 in captured.out
+        assert duty_8 in captured.out
+        assert duty_13 in captured.out
