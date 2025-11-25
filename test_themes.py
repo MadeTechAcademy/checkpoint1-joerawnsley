a2fde9c52d48707f4b02360f1e2878f70d827ae7
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-11-25 10:58:25 +0000

    removed unused list_of_duties and tests for it as it is no longer needed

--- test_themes.py
+++ test_themes.py
@@ -1,4 +1,3 @@
-# from themes import list_of_duties
 from themes import print_duties_to_terminal
 from themes import create_html_document
 from duties import *
@@ -12,16 +11,6 @@ duty_3 = "Duty 3 Engage in productive pair/mob programming to underpin the pract
 duty_7 = "Duty 2 Initiate and facilitate knowledge sharing and technical collaboration with teams and individuals, with a focus on supporting development of team members."
 duty_8 = "Duty 8 Evolve and define architecture, utilising the knowledge and experience of the team to design in an optimal user experience, scalability, security, high availability and optimal performance."
 duty_13 = "Duty 13 Accept ownership of changes; embody the DevOps culture of 'you build it, you run it', with a relentless focus on the user experience."
-        
-
-# class TestListOfDuties():
-#     def test_length(self):
-#         assert len(list_of_duties) == 13
-    
-#     def test_includes_duties(self):
-#         assert duty_3 in list_of_duties
-#         assert duty_8 in list_of_duties
-#         assert duty_13 in list_of_duties
 
 
 class TestPrintFunction():
