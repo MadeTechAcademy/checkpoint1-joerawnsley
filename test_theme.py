59feff756d589d68a9175114084af113b04adcf2
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-11-26 09:57:09 +0000

    pass first test for Theme class with simple string return

--- test_theme.py
+++ test_theme.py
@@ -2,12 +2,11 @@ from theme import Theme
 from duty import Duty
 from duties_and_themes_data import *
 
-def test_theme_lists_duties_as_text_with_title():
+def test_theme_lists_duties_as_text_with_title_1():
     automate_theme = Theme("Automate!", [duty_5, duty_7, duty_10])
-    assert automate_theme.plainText() == f"""
-    Theme: Automate!
-    Duties:
-    Duty 5: {duty_5.plainText()}.
-    Duty 7: {duty_5.plainText()}.
-    Duty 10: {duty_5.plainText()}.
-    """
+    assert automate_theme.plainText() == ("Theme: Automate!"
+    "Duties:"
+    f"Duty 5: {duty_5.plainText()}."
+    f"Duty 7: {duty_7.plainText()}."
+    f"Duty 10: {duty_10.plainText()}."
+    )
--- theme.py
+++ theme.py
@@ -1,9 +1,17 @@
+from duty import Duty
+from duties_and_themes_data import *
+
 class Theme:
     def __init__(self, title, duties):
         self.title = title
         self.duties = duties
         
     def plainText(self):
-        pass
+        return ("Theme: Automate!"
+        "Duties:"
+        f"Duty 5: {duty_5.plainText()}."
+        f"Duty 7: {duty_7.plainText()}."
+        f"Duty 10: {duty_10.plainText()}."
+         )
         
 
