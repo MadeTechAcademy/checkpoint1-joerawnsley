2a94ad9bdccce6e5a31fb3cd613749c54d16b308
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-11-26 11:04:53 +0000

    reafctor tests to avoid passing in complex formatted strings - I only want to test the contents, not the formatting

--- test_theme.py
+++ test_theme.py
@@ -2,11 +2,22 @@ from theme import Theme
 from duty import Duty
 from duties_and_themes_data import *
 
-def test_theme_lists_duties_as_text_with_title_1():
+def test_theme_plaintext_lists_duties_with_title():
     automate_theme = Theme("Automate!", [duty_5, duty_7, duty_10])
-    assert automate_theme.plainText() == ("Theme: Automate!\n\n"
-    "Duties:\n\n"
-    f"{duty_5.plainText()}\n"
-    f"{duty_7.plainText()}\n"
-    f"{duty_10.plainText()}"
-    )
+    automate_text_output = automate_theme.plainText()
+    assert duty_5.plainText() in automate_text_output
+    assert duty_7.plainText() in automate_text_output
+    assert duty_10.plainText() in automate_text_output
+    assert duty_1.plainText() not in automate_text_output
+    assert duty_11.plainText() not in automate_text_output
+    assert "Automate!" in automate_text_output
+    houston_theme = Theme("Houston, Prepare to Launch", [duty_6, duty_7, duty_10, duty_12])
+    houston_text_output = houston_theme.plainText()
+    assert duty_6.plainText() in houston_text_output
+    assert duty_7.plainText() in houston_text_output
+    assert duty_10.plainText() in houston_text_output
+    assert duty_12.plainText() in houston_text_output
+    assert duty_5.plainText() not in houston_text_output
+    assert duty_13.plainText() not in houston_text_output
+    assert "Houston, Prepare to Launch" in houston_text_output
+    assert "Automate!" not in houston_text_output
