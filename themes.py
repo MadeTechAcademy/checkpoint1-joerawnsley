d3ab171d6ec5ad7626335a1f8f15c88c462c442b
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-11-26 09:07:08 +0000

    create choose_theme and choose_output functions to allow user to choose their theme/output

--- themes.py
+++ themes.py
@@ -15,20 +15,47 @@ def create_html_document(filename, duties):
             html_file.write(duty.htmlListElement())
         html_file.write("</ul>")
 
+def choose_output(theme):
+    user_choice_of_output = input("""
+    Press (1) to list the duties in the terminal, or press (2) to see them in the browser.\n
+    Enter your choice:
+    """)
+    match user_choice_of_output:
+        case "1":
+            print("\n")
+            print_duties_to_terminal(theme)
+        case "2":
+            create_html_document(html_file_name, theme)
+            file_path = "file://" + os.path.realpath(html_file_name)
+            webbrowser.open(file_path)
+
+def choose_theme():
+    user_choice_of_theme = input("""
+    Please select a theme to see the duties for that theme.\n
+    (1) All duties
+    (2) Bootcamp
+    (3) Automate
+    (4) Houston, Prepare to Launch
+    (5) Going Deeper
+    (6) Assemble!
+    (7) Call Security
     
+    Enter your choice: """)
+    
+    match user_choice_of_theme:
+        case "1": choose_output(all_duties)
+        case "2": choose_output(bootcamp_duties)
+        case "3": choose_output(automate_duties)
+        case "4": choose_output(houston_duties)
+        case "5": choose_output(deeper_duties)
+        case "6": choose_output(assemble_duties)
+        case "7": choose_output(security_duties)
+
 
 if __name__=="__main__":
     
-    user_choice = input("""
-    Welcome to apprentice themes!\n
-    Press (1) to list all the duties, or press (2) to see them in the browser.\n
-    Enter your choice:
-    """)
-    if user_choice == '1':
-        print("\n")
-        print_duties_to_terminal(all_duties)
+    print("""
+    Welcome to APPRENTICE THEMES!""")
+    choose_theme()
     
-    if user_choice == '2':
-        create_html_document(html_file_name, all_duties)
-        file_path = "file://" + os.path.realpath(html_file_name)
-        webbrowser.open(file_path)
+   
